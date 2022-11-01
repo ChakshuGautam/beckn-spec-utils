be5933ca54e9219aff5be9b3b7ca3a83ea7fbc58
Yash Mittal <mittal.yash12@hotmail.com>
2022-11-01 23:38:48 +0530

    Fix: Fixed file not parsing completely
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const allKeys = new Set();
 // const specURL = "https://raw.githubusercontent.com/beckn/protocol-specifications/master/core/v0/api/core.yaml";
 const specURL = "https://raw.githubusercontent.com/beckn/DSEP-Specification/draft/api/dsep.yaml";
 
-$RefParser.dereference(specURL, (err, schema) => {
+$RefParser.dereference('./def.yaml', (err, schema) => {
     if (err) {
         console.error(err);
     }
     else {
         // save to file
         require("fs").writeFileSync("schema-deref.json", JSON.stringify(schema, null, 2));
-
         // finde paths
         const paths = Object.keys(schema.paths);
 
         // Iterate over paths and get all schemas
         paths.forEach(path => {
+            console.log('path: ', path);
             const methods = Object.keys(schema.paths[path]);
             methods.forEach(method => {
                 const operation = schema.paths[path][method];
@@ -32,26 +32,45 @@ $RefParser.dereference(specURL, (err, schema) => {
                         mediaTypes.forEach(mediaType => {
                             const schema = content[mediaType].schema;
                             const jsonSchemaWithErrors = toJsonSchema(schema, { dateToDateTime: true });
-
+                            
                             // remove keys of "format" with "#" in them
                             const flatJsonSchema = flatten(jsonSchemaWithErrors);
+                            // require("fs").writeFileSync(`flatten-json-schema-${path.substring(1)}.json`, JSON.stringify(flatJsonSchema, null, 2));
                             Object.keys(flatJsonSchema).forEach(key => {
+                                // deleting not required keys
+                                if(key == "type" || key.includes('required') || key.includes('$schema')) {
+                                    delete flatJsonSchema[key];
+                                    return;
+                                }
                                 if (key.includes("format") && (flatJsonSchema[key].includes("#/components/schemas/Item/properties/id") || flatJsonSchema[key].includes("phone"))) {
                                     flatJsonSchema[key] = "uuid";
                                 }
+                                // replacing the key with term 'properties' from schema after removing
+                                let newKey = null;
+                                if(key.includes('properties')) newKey = key.split('.').filter((val) => val !== 'properties').join('.') ;
+                                if(newKey && newKey.includes('message')) newKey = newKey.split('.').filter((val) => val !== 'message').join('.') ;
+                                if(newKey === "type") {
+                                    delete flatJsonSchema[key];
+                                    return;
+                                }
+                                if(newKey) {
+                                    flatJsonSchema[newKey] = flatJsonSchema[key] ;
+                                    delete flatJsonSchema[key];
+                                }
                             });
                             const jsonSchema = flatten.unflatten(flatJsonSchema);
+                            // require("fs").writeFileSync(`json-schema-${path.substring(1)}.json`, JSON.stringify(jsonSchema, null, 2));
+
                             try {
                                 const jsonSchemaString = JSON.stringify(jsonSchema, null, 2);
-                                const fakeData = jsf.generate(jsonSchema);
 
                                 // flatten json data to dot notation
-                                const flattened = flatten(fakeData);
-
+                                const jsonFlattened = flatten(jsonSchema);
                                 // get keys for flattened json
-                                const keys = Object.keys(flattened);
-
+                                // const keys = Object.keys(flattened);
+                                const keys = Object.keys(jsonFlattened);
                                 // remove keys not part of the schema
+
                                 const filteredKeys = keys.filter(key => {
                                     // split key into parts and get last part
                                     const parts = key.split('.');
@@ -87,6 +106,6 @@ $RefParser.dereference(specURL, (err, schema) => {
 
 
         // save all keys to new file csv format
-        require("fs").writeFileSync("keys.csv", Array.from(allKeys).join("\n"));
+        require("fs").writeFileSync(`keys-${Date.now()}.csv`, Array.from(allKeys).join("\n"));
     }
 });
